3c22c291faf36ba155f2c068d4152e36b4c764aa
Imane Belaid <belaidimane@gmail.com>
2023-09-09 00:27:08 +0100

    Completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,15 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+// import { trees, wildlife } from "./parks/RockyMountain";
+import * as RMFunctions from "./parks/RockyMountain";
+
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
-
+  console.log(RMFunctions.trees);
+  RMFunctions.wildlife();
+  
   return <h1>Colorado State Parks!</h1>;
 }
+
+export default ColoradoStateParks;
